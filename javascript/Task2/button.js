9b80af655a58e4cff0860926f69db7c239ccea78
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 03:39:44 +0200

    button element text added

--- javascript/Task2/button.js
+++ javascript/Task2/button.js
@@ -1,5 +1,6 @@
-//creating button element
+//creating and adding button element
 const button = document.createElement("button");
-
+//Adding texty to the button
+button.textContent = "Change Text";
 //Append the button to the body
 document.body.appendChild(button);
