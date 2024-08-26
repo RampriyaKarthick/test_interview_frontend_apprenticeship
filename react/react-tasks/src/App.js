4d821c1bdbca9bc476dd6e45bd41a01b34d83001
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 12:50:13 +0200

    adding input to the form

--- react/react-tasks/src/App.js
+++ react/react-tasks/src/App.js
@@ -5,22 +5,32 @@ import Greeting from './components/Greeting';
 
 function App() {
     const [name, setName] = useState('');
+    const [submittedName, setSubmittedName] = useState('');
 
     const handleInputChange = (event) => {
         setName(event.target.value);
     };
 
+    const handleSubmit = (event) => {
+      event.preventDefault(); 
+      setSubmittedName(name); 
+     
+  };
+
     return (
-        <div className="App">
-        <h1>Greeting App</h1>
-            <input 
-                type="text" 
-                value={name} 
-                onChange={handleInputChange} 
-                placeholder="Enter your name" 
-            />
-            <Greeting name={name} />
-        </div>
+      <div className="App">
+      <h1>Greeting App</h1>
+      <form onSubmit={handleSubmit}>
+          <input 
+              type="text" 
+              value={name} 
+              onChange={handleInputChange} 
+              placeholder="Enter your name" 
+          />
+          <button type="submit">Submit</button>
+      </form>
+      <Greeting name={submittedName} />
+  </div>
     );
 }
 
