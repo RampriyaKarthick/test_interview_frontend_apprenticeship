8e53959b0e954c4168c9829a38af9749af3629dc
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 05:33:17 +0200

    first five todos displayed in browser

--- javascript/Task 3/basicFetch.js
+++ javascript/Task 3/basicFetch.js
@@ -4,17 +4,28 @@ async function fetchTodos() {
     try{
 const response = await fetch("https://jsonplaceholder.typicode.com/todos")
 if (response.ok){
-    const todos = await response.json(); //conveting response to json
+    const todos = await response.json(); //converting response to json
 
     //To get first five todos from data
     const firstFiveTodos = todos.slice(0, 5);
     console.log(firstFiveTodos)
+    const unOrderedList = document.createElement("ul");
+    firstFiveTodos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.title;
+        unOrderedList.appendChild(li);
+
+        const fiveTodos = document.getElementById('todo-list-container');
+        fiveTodos.appendChild(unOrderedList);
+    });
 }
 else {
     throw new Error('Bad Network response');
 }
     }
-    catch{
+    catch(error){
         console.error('fetch Data Error:', error);
     }
 }
+
+fetchTodos(); //function call
