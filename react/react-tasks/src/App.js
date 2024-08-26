f1018a827c5d0fefb293b656c738dc9d5ed307cb
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 12:36:39 +0200

    lifting th e state to App passing a prop to Greeting to App

--- react/react-tasks/src/App.js
+++ react/react-tasks/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import Greeting from './components/Greeting';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('');
+
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <input 
+                type="text" 
+                value={name} 
+                onChange={handleInputChange} 
+                placeholder="Enter your name" 
+            />
+            <Greeting name={name} />
+        </div>
+    );
 }
 
 export default App;
