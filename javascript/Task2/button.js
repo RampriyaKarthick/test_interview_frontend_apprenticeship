43f3dd15f42fb3242ecf04e0c6627e4cb8d6a66e
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 03:43:08 +0200

    adding h1 tag to the body

--- javascript/Task2/button.js
+++ javascript/Task2/button.js
@@ -1,6 +1,16 @@
+//creating and adding ht e h1 element
+const text = document.createElement("h1");
+//Adding texty to h1
+text.textContent = "myText";
+//Append the h1 to the body
+document.body.appendChild(text);
+
+
+
 //creating and adding button element
 const button = document.createElement("button");
 //Adding texty to the button
 button.textContent = "Change Text";
 //Append the button to the body
 document.body.appendChild(button);
+
