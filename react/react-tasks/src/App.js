796cebf9bf301bf62d3cb0af619d2b888991a6f4
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 17:11:35 +0200

    Name List

--- react/react-tasks/src/App.js
+++ react/react-tasks/src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import "./styles/greeting.css"
 import Greeting from "./components/Task 1/Greeting"
 import Counter from "./components/Task 2/Counter"
+import NameListRender from './components/Task 3/NameListRender';
 import "./App.css"
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
      <h1 className="tasks">Check React Tasks</h1> 
     <Greeting/>
     <Counter/>
+    <NameListRender/>
      
   </div>
     );
---
+++ react/react-tasks/src/components/Task 3/NameListRender.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NameListRender() {
+
+    const givennamesArray = ['Alice', 'Bob', 'Charlie', 'David'];
+  return (
+    <div>
+        <h1 className='greeting'>Task 3 - Name List</h1>
+        
+    </div>
+  )
+}
+
+export default NameListRender
