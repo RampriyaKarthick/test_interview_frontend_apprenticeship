ecd64c1af14142f8e44c1e03745410b08c7130ad
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 03:13:55 +0200

    comment line included getEvenNumbers function

--- javascript/Task 1/getEvenNumbers.js
+++ javascript/Task 1/getEvenNumbers.js
@@ -1,7 +1,7 @@
 // There are various methods to approach this task using filter method would be the best solution for clarity and efficiency
 
 function getEvenNumbers(a){         //function definition
-    return a.filter(number => number%2===0)   //callback function
+    return a.filter(number => number%2===0)   //callback function and %operator checks for reminders for the number divisible by 2, if true the number is included in the new array else excluded
 }
 
 const inputArray = [5,7,10,55,78,24,1,3]   // Defining array of numbers
