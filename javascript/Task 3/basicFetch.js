b2ed7a13d29ecb08bbef9dd6a55d842e5273b834
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-08-26 04:58:14 +0200

    using fetch to fetch data from API

--- javascript/Task 3/basicFetch.js
+++ javascript/Task 3/basicFetch.js
@@ -1,8 +1,13 @@
+//Since the task is small fetch to make API calls would be the good option 
+//Else, I prefer axios for its additional features and convenience.
 async function fetchTodos() {
     try{
-
+const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+if (response.ok){
+    const data = await response.json(); //conveting response to json
+}
     }
     catch{
-        
+
     }
 }
